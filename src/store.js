bd71d9130b48331a1c99c05496dcd922be221852
Pavel German <frontendue@gmail.com>
2023-07-16 20:30:14 +1000

    --prod

--- src/store.js
+++ src/store.js
@@ -1,22 +1,19 @@
 import { legacy_createStore as createStore, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 
 import rootReducer from './reducers/index';
 
 const composeEnhancers =
   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-        // Specify extension’s options like name, actionsDenylist, actionsCreators, serialize...
-      })
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
     : compose;
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  const result = next(action);
-  console.log(store.getState());
-  return result;
-};
+// const loggerMiddleware = (store) => (next) => (action) => {
+//   const result = next(action);
+//   console.log(store.getState());
+//   return result;
+// };
 
-const enhancer = composeEnhancers(applyMiddleware(loggerMiddleware, thunk));
+const enhancer = composeEnhancers(applyMiddleware());
 
 const store = createStore(rootReducer, enhancer);
 
